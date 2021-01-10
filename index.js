0baa8c6c722c7dba287e8e9c2f9a968754140694
Gabriela Zalta <gzalta96@gmail.com>
2021-01-10 10:08:23 -0500

    html implemented into application

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//logic
-
-// const { start } = require("repl");
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const bootstrap = require('bootstrap');
@@ -280,12 +276,118 @@ function newIntern() {
 function completeRoster() {
     console.log("************************************ You have completed your team roster! ************************************")
 
-    // const html = 
+    const htmlArray = [];
 
+    const html = `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>My Team</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h1 class="text-center text-white bg-danger h-100 fs-3 m-4 pb-2 pt-5">My Team</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container m-6 p-5">
+
+        <div class="row row-cols-3 justify-content-md-center">`
+
+    htmlArray.push(html);
+
+    for (let i = 1; i > teamRoster.length; i++) {
+        let card = "";
+
+        if (teamRoster[i].officeNumber) {
+            card = `
+                <div class="col">
+                <div class="card text-white bg-primary p-2 m-5">
+                    <div class="card-body">
+                        <h5 class="card-title">${newManager.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-white"><i class="fas fa-mug-hot"></i> Manager
+                        </h6>
+                        <div class="card text-dark">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${newManager.id}</li>
+                                <li class="list-group-item">Email: ${newManager.email}</li>
+                                <li class="list-group-item">Office Number: ${newManager.officeNumber}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> `
+        }
+
+        if (teamRoster[i].github) {
+            card = `
+                <div class="col">
+                <div class="card text-white bg-primary p-2 m-5">
+                    <div class="card-body">
+                        <h5 class="card-title">${newEngineer.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-white"> <i class="fas fa-glasses"></i> Engineer</h6>
+                        <div class="card text-dark">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${newEngineer.id}</li>
+                                <li class="list-group-item">Email: ${newEngineer.email}</li>
+                                <li class="list-group-item">GitHub: ${newEngineer.github}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> `
+        }
+
+        if (teamRoster[i].school) {
+            card = `
+                <div class="col">
+                <div class="card text-white bg-primary p-2 m-5">
+                    <div class="card-body">
+                        <h5 class="card-title">${newIntern.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-white"><i class="fas fa-user-graduate"></i> Intern</h6>
+                        <div class="card text-dark">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${newIntern.id}</li>
+                                <li class="list-group-item">Email: ${newIntern.email}</li>
+                                <li class="list-group-item">School: ${newIntern.school}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> `
+        }
+
+        card += `
+            </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
+            </script>
+        </body>   
+        </html> `
+
+        htmlArray.push(card);
+    }
+
+
+    fs.writeFile("./dist/roster.html", htmlArray.join(""), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+    console.log("Your team roster has been succesfully created!");
 };
 
 start();
-
-
-//create the webpage with the manager (how you will design will be like readmegen function with filable vars (m.email , m.getRole()))
-//if you are not done, then we ask for startfx start()
